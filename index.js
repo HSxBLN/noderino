13af998d9c7967a7addc3ebf5c83aaf4b4563f91
hdevopsiron <hauke-schulte@web.de>
2025-11-20 17:29:54 +0100

    azure test

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require("express")()
+require("dotenv").config()
+
+const desiredPath = process.env.DESIRED_PATH || "/"
+const port = process.env.PORT || 8080
+const number = process.env.NUMBER || 0
+
+app.get(desiredPath, (req, res) => {
+  res.send(`<h1>Hello from ${desiredPath} number ${number}!</h1>`)
+})
+
+app.get("/healthcheck", (req, res) => {
+    res.status(200).send("It works!")
+})
+
+app.listen(port, () => {
+  console.log(`🚀 Server ${number} listening on ${port} at path ${desiredPath}`)
+})
